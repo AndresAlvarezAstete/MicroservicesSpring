8260b99161274a7c1cde816998ad1f8ec70ea2d0
Andres <andres.alvarez.as@gmail.com>
2022-12-21 17:59:22 -0300

    commit

---
+++ springboot-servicio-productos/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO productos (nombre, marca, precio,  create_at) VALUES('Televisor', 'Panasonic', 800, NOW());
+INSERT INTO productos (nombre, marca, precio,  create_at) VALUES('Camara', 'Sony', 700, NOW());
+INSERT INTO productos (nombre, marca, precio,  create_at) VALUES('Celular', 'Apple', 1000, NOW());
+INSERT INTO productos (nombre, marca, precio,  create_at) VALUES('Notebook', 'Sony', 1000, NOW());
+INSERT INTO productos (nombre, marca, precio,  create_at) VALUES('Computador', 'Hewlett Packard', 500, NOW());
+INSERT INTO productos (nombre, marca, precio,  create_at) VALUES('Bicicleta', 'Bianchi', 600, NOW());
+INSERT INTO productos (nombre, marca, precio,  create_at) VALUES('Zapatilla', 'Nike', 100, NOW());
+INSERT INTO productos (nombre, marca, precio,  create_at) VALUES('Jockey', 'Adidas', 200, NOW());
+INSERT INTO productos (nombre, marca, precio,  create_at) VALUES('Pantalon', 'Reebok', 300, NOW());
